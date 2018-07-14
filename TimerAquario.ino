31946b902271e61aea245ad9a2f041ec31f14819
Gabriel Kaffka <kaffka@gmail.com>
2018-07-14 20:22:55 +0000

    Adds support to sun api

--- TimerAquario.ino
+++ TimerAquario.ino
@@ -1,8 +1,10 @@
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include <time.h>
 
-const char* ssid = "ssid";
-const char* password = "psw";
+const char* ssid = "Capiroto";
+const char* password = "hal90000";
 
 int timezone = 3;
 int dst = 0;
@@ -10,6 +12,7 @@ int dst = 0;
 void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
+
   pinMode(13, OUTPUT);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -29,13 +32,39 @@ void setup() {
 }
 
 void loop() {
+  int sunrise = 9;
+  int sunset = 20;
+  HTTPClient http;  //Declare an object of class HTTPClient
+  http.begin("http://sunapi.herokuapp.com/day");  //Specify request destination
+  int httpCode = http.GET();//Send the request
+  Serial.println("code: " + httpCode);
+  if (httpCode > 0) { //Check the returning code
+    const size_t bufferSize = JSON_OBJECT_SIZE(3) + 60;
+    DynamicJsonBuffer jsonBuffer(bufferSize);
+    JsonObject& root = jsonBuffer.parseObject(http.getString());
+    const char* sunrise_c = root["sunrise"]; // "1"
+    const char* sunset_c = root["sunset"]; // "23"
+    sunrise = atol( sunrise_c );
+    sunset = atol (sunset_c);
+    Serial.print("sunrise: ");
+    Serial.println(sunrise);
+    Serial.print("sunset: ");
+    Serial.println(sunset);
+  }
+  http.end();   //Close connection
+
   time_t now = time(nullptr);
   struct tm * timeinfo;
   time(&now);
   timeinfo = localtime(&now);
-
+  if (sunrise == 0) {
+    sunrise = 9;
+  }
+  if (sunset == 0) {
+    sunset = 20;
+  }
   int a = (int) timeinfo->tm_hour;
-  if (a >= 8 && a < 20) {
+  if (a >= sunrise && a < sunset) {
     digitalWrite(13, HIGH);
   } else {
     digitalWrite(13, LOW);
